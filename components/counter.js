ed936a903fcebf7f1ac836f1988d1a1fe20328c9
AMA Mazing <alexmalone489@gmail.com>
2023-01-18 19:51:06 +1100

    update

--- components/counter.js
+++ components/counter.js
@@ -4,15 +4,6 @@ import React, { Component, useRef, useState, useLayoutEffect, useEffect } from "
 
 const Counter=()=>{
     const ref = useRef(null);
-    const [width, setWidth] = useState(0);
-
-    useEffect(() => {
-        const observer = new ResizeObserver(entries => {
-          setWidth(entries[0].contentRect.width)
-        })
-        observer.observe(ref.current)
-        return () => ref.current && observer.unobserve(ref.current)
-      }, [])
 
     const [count, setCount] = useState(0);
  
@@ -28,20 +19,12 @@ const Counter=()=>{
         setCount(0);
     }
 
-    const countLen = count.toString().length;
-    const equation = (140 + (80 * (countLen - 1)));
-
-    if ((140 + (80 * (countLen - 1))) > width) {
-        var bbc = "too big";
-    } else {
-        var bbc = "not that big";
-    }
      
 
     return (
         <div>
-            {bbc} <br /> {countLen}
-            <div ref={ref} class="box" style={{fontSize: '5rem', padding: '60px', width: '170px'}} id="box" >{count}</div> 
+            <br />
+            <div ref={ref} class="box" style={{fontSize: '5rem', padding: '60px'}} id="box" >{count}</div> 
 
             <button onClick={reset} class="smallbox reset">Reset</button>
             <button onClick={decrease} class="smallbox minus">-</button>
